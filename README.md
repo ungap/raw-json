d8da6fafedc96d8fd790ed2c9ad29d86edbdb9fa
webreflection <andrea.giammarchi@gmail.com>
2024-10-09 15:57:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# raw-json
+
+A [JSON.rawJSON](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/rawJSON) and [JSON.isRawJSON](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/isRawJSON) ponyfill (meaning: no global patch applied).
+
+```js
+import * as JSON from '@ungap/raw-json';
+
+JSON.stringify({
+  a: JSON.rawJSON('12345678901234567890'),
+  b: JSON.rawJSON(12345678901234567890n),
+  c: JSON.rawJSON('null'),
+  d: JSON.rawJSON(null),
+  e: JSON.rawJSON(123),
+  f: JSON.rawJSON('"hello raw JSON"'),
+  g: JSON.rawJSON(true),
+  h: JSON.rawJSON(false)
+});
+```
---
+++ index.js
@@ -0,0 +1,64 @@
+let { parse, stringify, rawJSON, isRawJSON } = JSON;
+
+if (!rawJSON) {
+  // isRawJSON
+  const { freeze, setPrototypeOf } = Object;
+  class RawJSON {
+    static isRawJSON(o) {
+      return typeof o === 'object' && !!o && #rawJSON in o;
+    }
+    #rawJSON = true;
+    constructor(rawJSON) {
+      this.rawJSON = rawJSON;
+      freeze(setPrototypeOf(this, null));
+    }
+  }
+  ({ isRawJSON } = RawJSON);
+
+  // rawJSON
+  rawJSON = value => {
+    const type = typeof value;
+    switch (type) {
+      case 'object':
+        if (value) break;
+      case 'bigint':
+      case 'boolean':
+      case 'number':
+        value = String(value);
+      case 'string':
+        parse(value);
+        return new RawJSON(value);
+    }
+    throw new TypeError('Unexpected ' + type);
+  };
+
+  // stringify
+  const { isArray } = Array;
+  const noop = (_, value) => value;
+  const fix = (replacer, raws) => {
+    let id = 0;
+    const suffix = Math.random();
+    const $ = isArray(replacer) ?
+      (k, v) => (k === '' || replacer.includes(k) ? v : void 0) :
+      (replacer || noop);
+    return function (key, value) {
+      value = $.call(this, key, value);
+      if (isRawJSON(value)) {
+        const { rawJSON } = value;
+        value = [++id + suffix].join('\x00');
+        raws.set(value, rawJSON);
+      }
+      return value;
+    };
+  };
+  const s = stringify;
+  stringify = (value, replacer, space) => {
+    const raws = new Map;
+    const result = s(value, fix(replacer, raws), space);
+    const re = new RegExp(`"(${[...raws.keys()].join('|')})"`, 'g');
+    const place = (_, key) => raws.get(key);
+    return result.replace(re, place);
+  };
+}
+
+export { parse, stringify, rawJSON, isRawJSON };
