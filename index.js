1c8bbfd4e92694446c38a68e377dec7d1dfecb02
webreflection <andrea.giammarchi@gmail.com>
2024-10-15 10:37:55 +0200

    Explicit String.replace

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (!rawJSON) {
     if (typeof reviver === 'function') {
       const $ = reviver;
       const a = [];
-      text = text.replace(
+      text = String(text).replace(
         // parse all primitives in one go: string | number | boolean | null
         /(?:"(?:(?=(\\?))\1.)*?"\s*:?|[0-9eE.-]+|true|false|null)/g,
         $ => $.at(-1) !== ':' ? ($[0] === '"' ? (a.push($) - 1) : `"${$}"`) : $
